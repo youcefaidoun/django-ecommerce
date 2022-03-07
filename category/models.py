b4ec85390f648e4b16c3f77b51132840d5be26a7
youcefaidoun <youcefaidoun16@gmail.com>
2022-03-07 15:57:08 +0100

    commit

---
+++ category/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(_('title'),max_length=50, unique=True)
+    slug =  models.SlugField(_('slug'),max_length=100, unique=True)
+    description = models.TextField(_('description'),max_length=200, blank=True)
+    image = models.ImageField(_('image'),upload_to='images/categories', blank=True)
+
+    class Meta:
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
